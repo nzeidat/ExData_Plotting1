06cd124286042e41fd0dfcd30139558dea42ffb2
nzeidat <nzeidat@gmail.com>
2015-07-10 12:26:42 -0500

    first plot for project 1

---
+++ plot2.R
@@ -0,0 +1,24 @@
+install.packages("lubridate")
+library(lubridate)
+
+## Read in the data
+epc <- read.table(file="household_power_consumption.txt", 
+                  sep=";", header=TRUE, na.strings=c("?"))
+
+## subset only the 2 days we are interested in
+req_Data <- subset(epc, epc$Date == '1/2/2007' |epc$Date == '2/2/2007', select = c(Date, Time, Global_active_power))
+Updated_rd <- cbind(req_Data, DateTime = dmy(req_Data$Date) + hms(req_Data$Time))
+
+## Open the png device
+png(filename="plo2.png")
+##Plot the data
+
+with(Updated_rd, 
+     plot(DateTime, 
+          Global_active_power, 
+          type="l",
+          xlab="",
+          ylab="Global Active Power (kilowatts)"))
+
+## close the Graphics device
+dev.off()
