1be72ccb4aeba946448bf4588ead1deae0fcf14e
nzeidat <nzeidat@gmail.com>
2015-07-09 19:57:23 -0500

    first plot for project 1

---
+++ plot1.R
@@ -0,0 +1,18 @@
+## plot1.R
+## Read in the data
+epc <- read.table(file="household_power_consumption.txt", 
+                  sep=";", header=TRUE, na.strings=c("?"))
+
+## subset only the 2 days we are interested in
+required_data <- subset(epc, epc$Date == '1/2/2007' |epc$Date == '2/2/2007')
+
+## Open the png device
+png(filename="/c/Users/Nidals/datasciencecoursera/courses/04_ExploratoryAnalysis/ExData_Plotting1/ExData_Plotting1/plo1.png")
+##Plot the data
+hist(required_data$Global_active_power, 
+              col="red",
+              main="Global Active Power",
+              xlab="Global Active Power (kilowatts)",
+              ylab="Frquency")
+## close the Graphics device
+dev.off()
